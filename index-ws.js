75d78397e63b81a0ac3800ffda61f84b6f4c73f1
Muhammad Awais <muhammad.awais.20024@gmail.com>
2024-04-20 16:56:12 +0500

    Using websockets with express

---
+++ index-ws.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const server = require("http").createServer();
+const app = express();
+
+app.get("/", function (req, res) {
+  res.sendFile("index.html", { root: __dirname });
+});
+
+server.on("request", app);
+server.listen(3000, function () {
+  console.log("server started on port 3000");
+});
