f16a742a305fdb8acd8eb979af4e7e92a4bfcce2
Muhammad Awais <muhammad.awais.20024@gmail.com>
2024-04-20 18:19:44 +0500

    Creating a web socket using express and ws

--- index-ws.js
+++ index-ws.js
@@ -10,3 +10,28 @@ server.on("request", app);
 server.listen(3000, function () {
   console.log("server started on port 3000");
 });
+
+// Begin websocket
+const WebSocketServer = require("ws").Server;
+
+const wss = new WebSocketServer({ server });
+
+wss.on("connection", function connection(ws) {
+  const numOfClients = wss.clients.size;
+  console.log(`Clients connected ${numOfClients}`);
+  wss.broadcast(`Current visitors: ${numOfClients}`);
+
+  if (ws.readyState == ws.OPEN) {
+    ws.send("Welcome to my server");
+  }
+  ws.on("close", function close() {
+    wss.broadcast(`Current visitors: ${numOfClients}`);
+    console.log("A client has just disconnected");
+  });
+});
+
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach(function each(client) {
+    client.send(data);
+  });
+};
