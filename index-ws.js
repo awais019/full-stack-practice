5a3849676c9652db552b6cec64037a0b6c816a09
Muhammad Awais <muhammad.awais.20024@gmail.com>
2024-04-22 11:49:16 +0500

    Added db

--- index-ws.js
+++ index-ws.js
@@ -11,6 +11,15 @@ server.listen(3000, function () {
   console.log("server started on port 3000");
 });
 
+process.on("SIGINT", () => {
+  wss.clients.forEach(function each(client) {
+    client.close();
+  });
+  server.close(() => {
+    shutdownDB();
+  });
+});
+
 // Begin websocket
 const WebSocketServer = require("ws").Server;
 
@@ -24,6 +33,11 @@ wss.on("connection", function connection(ws) {
   if (ws.readyState == ws.OPEN) {
     ws.send("Welcome to my server");
   }
+
+  db.run(`INSERT INTO visitors (count, time)
+        VALUES (${numOfClients}, datetime('now'))
+   `);
+
   ws.on("close", function close() {
     wss.broadcast(`Current visitors: ${numOfClients}`);
     console.log("A client has just disconnected");
@@ -35,3 +49,30 @@ wss.broadcast = function broadcast(data) {
     client.send(data);
   });
 };
+
+// end websocket
+
+// begin database
+const sqlite = require("sqlite3");
+const db = new sqlite.Database(":memory:");
+
+db.serialize(() => {
+  db.run(`
+    CREATE TABLE visitors ( 
+      count INTEGER,
+      time TEXT
+    )
+  `);
+});
+
+function getCounts() {
+  db.each("SELECT * FROM visitors", (err, row) => {
+    console.log(row);
+  });
+}
+
+function shutdownDB() {
+  getCounts();
+  console.log("Shutting down db");
+  db.close();
+}
