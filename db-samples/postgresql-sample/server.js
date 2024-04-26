4bde238924e6624f756fe7f051720a5611f676e7
Muhammad Awais <muhammad.awais.20024@gmail.com>
2024-04-26 21:10:45 +0500

    postgres sample

---
+++ db-samples/postgresql-sample/server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  connectionString:
+    "postgresql://postgres:mysecretpassword@localhost:5432/message_boards",
+});
+
+async function init() {
+  const app = express();
+
+  app.get("/get", async (req, res) => {
+    const client = await pool.connect();
+    const [commentRes, boardRes] = await Promise.all([
+      client.query(
+        "SELECT * FROM comments NATURAL LEFT JOIN rich_content WHERE board_id = $1",
+        [req.query.search]
+      ),
+      client.query("SELECT * FROM boards WHERE board_id = $1", [
+        req.query.search,
+      ]),
+    ]);
+
+    res.json({
+      status: "ok",
+      board: boardRes.rows[0] || {},
+      comment: commentRes.rows,
+    });
+  });
+  const PORT = 3000;
+  app.use(express.static("./static"));
+  app.listen(PORT, () => {
+    console.log(`server started on port ${PORT}`);
+  });
+}
+
+init();
